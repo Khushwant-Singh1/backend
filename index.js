68a39247796597587bbcd0adb276f58179b5ceec
Khushwant Singh <150510696+Khushwant-Singh1@users.noreply.github.com>
2024-09-22 19:33:00 +0000

    second

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/twitter', (req, res)=>{
+    res.send('khushwantzx@gmail.com')
+})
+
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>this is the h1 of my first backend</h1>')
+})
+
+app.get('/god',(req,res)=>{
+    res.send('<h2>Khushwant Singh</h2>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
